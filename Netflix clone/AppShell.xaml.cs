90f067eb3c3ab55ae04014bd05c32ead9ac1605d
ontash <ontash@ONI-LT-20>
2024-11-26 09:20:04 +0530

    Add project files.

---
+++ Netflix clone/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Netflix_clone
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Netflix clone/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Netflix_clone
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
